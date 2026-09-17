aa7baa74083c1a9451f9f12236421c4551a1c5a0
sophieandonov <soa00044@laurea.fi>
2026-09-17 10:53:04 +0300

    js testaus

---
+++ Workshop-2/script.js
@@ -0,0 +1,17 @@
+console.log ("Hello, World!");
+
+const userName = "Sophie";
+const favoriteAnimal = "Giraffe";
+
+console.log("My name is " + userName);
+console.log("My favorite animal is " + favoriteAnimal);
+
+alert("Tervetuloa nettisivuilleni! Tässä on kuva lempieläimestäni, kirahvista.");
+
+const visitorName = prompt("Mikä on nimesi?");
+
+console.log(visitorName);
+
+console.log("Terve " + visitorName + "!Tervetuloa JavaScriptin pariin!");
+
+const visitorfavoriteAnimal = prompt("Mikä on lempieläimesi?");
